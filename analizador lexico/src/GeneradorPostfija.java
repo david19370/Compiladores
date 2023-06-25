a26e57e5219cd0739275f9865387f1bd34b4fc3e
David López <dlopezl1901@alumno.ipn.mx>
2023-06-24 19:08:11 -0600

    a

---
+++ analizador lexico/src/GeneradorPostfija.java
@@ -0,0 +1,115 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class GeneradorPostfija {
+
+    private final List<Token> infija;
+    private final Stack<Token> pila;
+    private final List<Token> postfija;
+
+    public GeneradorPostfija(List<Token> infija) {
+        this.infija = infija;
+        this.pila = new Stack<>();
+        this.postfija = new ArrayList<>();
+    }
+
+    public List<Token> convertir(){
+        boolean estructuraDeControl = false;
+        Stack<Token> pilaEstructurasDeControl = new Stack<>();
+
+        for(int i=0; i<infija.size(); i++){
+            Token t = infija.get(i);
+
+            if(t.tipo == TipoToken.EOF){
+                break;
+            }
+
+            if(t.esPalabraReservada()){
+                /*
+                 Si el token actual es una palabra reservada, se va directo a la
+                 lista de salida.
+                 */
+                postfija.add(t);
+                if (t.esEstructuraDeControl()){
+                    estructuraDeControl = true;
+                    pilaEstructurasDeControl.push(t);
+                }
+            }
+            else if(t.esOperando()){
+                postfija.add(t);
+            }
+            else if(t.tipo == TipoToken.PARENTESIS_DERECHO){
+                pila.push(t);
+            }
+            else if(t.tipo == TipoToken.PARENTESIS_IZQUIERDO){
+                while(!pila.isEmpty() && pila.peek().tipo != TipoToken.PARENTESIS_DERECHO){
+                    Token temp = pila.pop();
+                    postfija.add(temp);
+                }
+                if(pila.peek().tipo == TipoToken.PARENTESIS_DERECHO){
+                    pila.pop();
+                }
+                if(estructuraDeControl){
+                    postfija.add(new Token(TipoToken.PUNTO_COMA, ";", null));
+                }
+            }
+            else if(t.esOperador()){
+                while(!pila.isEmpty() && pila.peek().precedenciaMayorIgual(t)){
+                    Token temp = pila.pop();
+                    postfija.add(temp);
+                }
+                pila.push(t);
+            }
+            else if(t.tipo == TipoToken.PUNTO_COMA){
+                while(!pila.isEmpty() && pila.peek().tipo != TipoToken.LLAVE_DERECHA){
+                    Token temp = pila.pop();
+                    postfija.add(temp);
+                }
+                postfija.add(t);
+            }
+            else if(t.tipo == TipoToken.LLAVE_DERECHA){
+                // Se mete a la pila, tal como el parentesis. Este paso
+                // pudiera omitirse, sólo hay que tener cuidado en el manejo
+                // del "}".
+                pila.push(t);
+            }
+            else if(t.tipo == TipoToken.LLAVE_IZQUIERDA && estructuraDeControl){
+
+                // Primero verificar si hay un else:
+                if(infija.get(i + 1).tipo == TipoToken.ADEMAS){
+                    // Sacar el "{" de la pila
+                    pila.pop();
+                }
+                else{
+                    // En este punto, en la pila sólo hay un token: "{"
+                    // El cual se extrae y se añade un ";" a cadena postfija,
+                    // El cual servirá para indicar que se finaliza la estructura
+                    // de control.
+                    pila.pop();
+                    postfija.add(new Token(TipoToken.PUNTO_COMA, ";", null));
+
+                    // Se extrae de la pila de estrucuras de control, el elemento en el tope
+                    pilaEstructurasDeControl.pop();
+                    if(pilaEstructurasDeControl.isEmpty()){
+                        estructuraDeControl = false;
+                    }
+                }
+
+
+            }
+        }
+        while(!pila.isEmpty()){
+            Token temp = pila.pop();
+            postfija.add(temp);
+        }
+
+        while(!pilaEstructurasDeControl.isEmpty()){
+            pilaEstructurasDeControl.pop();
+            postfija.add(new Token(TipoToken.PUNTO_COMA, ";", null));
+        }
+
+        return postfija;
+    }
+
+}
--- analizador lexico/src/Token.java
+++ analizador lexico/src/Token.java
@@ -22,4 +22,89 @@ public class Token {
     public String toString(){
         return tipo + " " + lexema + " ";
     }
+
+
+    // Métodos auxiliares
+    public boolean esOperando(){
+        switch (this.tipo){
+            case IDENTIFICADOR:
+            case NUMERO:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public boolean esOperador(){
+        switch (this.tipo){
+            case SUMA:
+            case RESTA:
+            case MULTIPLICACION:
+            case DIVISION:
+            case IGUAL_QUE:
+            case MAYOR_QUE:
+            case OP_MAYOR_IGUAL_QUE:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public boolean esPalabraReservada(){
+        switch (this.tipo){
+            case VAR:
+            case SI:
+            case IMPRIMIR:
+            case ADEMAS:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public boolean esEstructuraDeControl(){
+        switch (this.tipo){
+            case SI:
+            case ADEMAS:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public boolean precedenciaMayorIgual(Token t){
+        return this.obtenerPrecedencia() >= t.obtenerPrecedencia();
+    }
+
+    private int obtenerPrecedencia(){
+        switch (this.tipo){
+            case MULTIPLICACION:
+            case DIVISION:
+                return 3;
+            case SUMA:
+            case RESTA:
+                return 2;
+            case IGUAL_QUE:
+                return 1;
+            case MAYOR_QUE:
+            case OP_MAYOR_IGUAL_QUE:
+                return 1;
+        }
+
+        return 0;
+    }
+
+    public int aridad(){
+        switch (this.tipo) {
+            case MULTIPLICACION:
+            case DIVISION:
+            case SUMA:
+            case RESTA:
+            case IGUAL_QUE:
+            case MAYOR_QUE:
+            case OP_MAYOR_IGUAL_QUE:
+                return 2;
+        }
+        return 0;
+    }
 }
